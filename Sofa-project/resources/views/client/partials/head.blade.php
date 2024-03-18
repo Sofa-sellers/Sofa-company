3518ef5c91019af677143b2f47ed5279d7c2857c
firestorm1st <nhatnam99005@gmail.com>
2024-03-18 18:10:22 +0700

    masterLayout_18/3

---
+++ Sofa-project/resources/views/client/partials/head.blade.php
@@ -0,0 +1,11 @@
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Moller - Furniture & Decor eCommerce Responsive Bootstrap5 Template</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('client/assets/images/favicon.ico')}}" />
+    <link rel="stylesheet" href="{{asset('client/assets/css/vendor/bootstrap.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('client/assets/css/vendor/ionicons.css')}}" />
+    <link rel="stylesheet" href="{{asset('client/assets/css/plugins/animate.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('client/assets/css/plugins/swiper-bundle.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('client/assets/css/plugins/jquery-ui.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('client/assets/css/style.css')}}" />
