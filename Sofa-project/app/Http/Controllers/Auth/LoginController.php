aa2138248ba3b0180cb14f5efbc0a6d8ab9bdf45
firestorm1st <nhatnam99005@gmail.com>
2024-03-20 14:24:53 +0700

    gộp toàn bộ client và login vào LoginController

--- Sofa-project/app/Http/Controllers/Auth/LoginController.php
+++ Sofa-project/app/Http/Controllers/Auth/LoginController.php
@@ -15,12 +15,53 @@ class LoginController extends Controller
         return view('guest.pages.login');
     }
 
-    public function register(Request $request){
+    public function ratingCommentStore(StoreRequest $request){
 
     }
 
-    public function register(Request $request){
-
+    public function ratingCommentUpdate(UpdateRequest $request, $id){
+//
     }
-    
+
+    public function checkout(Request $request){
+        //
+    }
+
+    public function accountIndex(){
+        return view('client.pages.account');
+    }
+
+    public function addToWishlist($id, $quantity){
+        //
+    }
+
+    public function showWishlist(){
+        return view('client.modules.wishlist');
+        
+    }
+
+    public function wishlistDelete($id){
+        //
+    }
+
+    public function wishlistUpdate(UpdateRequest $request, $id){
+//
+    }
+
+    public function order(Request $request){
+        //
+    }
+
+    public function addressUpdate(UpdateRequest $request, $id){
+//
+    }
+
+    public function accountDetailsUpdate(UpdateRequest $request, $id){
+//
+    }
+
+    public function logout(){
+        //
+    }
+
 }
