478e2d9398ca423e0802d245f379a915e2707c55
firestorm1st <nhatnam99005@gmail.com>
2024-03-30 16:35:14 +0700

    pagination and shop page 30/3

--- Sofa-project/app/Http/Controllers/Guest/GuestController.php
+++ Sofa-project/app/Http/Controllers/Guest/GuestController.php
@@ -25,12 +25,60 @@ class GuestController extends Controller
         ]);
     }
 
-    public function viewShop(){
-        return view('guest.shop');
+    public function viewShop(Request $request){
+        $page=$request->query("page");
+        $size=$request->query("size");
+
+        if(!$page){
+            $page=1;
+        }
+        if(!$size){
+            $size=9;
+        }
+        $order=$request->query("order");
+        if(!$order){
+            $order=-1;
+        }
+        $o_column="";
+        $o_order="";
+        switch($order){
+            case 1:
+                $o_column="created_at";
+                $o_order="DESC";
+                break;
+            case 2:
+                $o_column="created_at";
+                $o_order="ASC";
+                break;    
+            case 3:
+                $o_column="sale_price";
+                $o_order="ASC";
+                break;
+            case 4:
+                $o_column="sale_price";
+                $o_order="DESC";
+                break;
+            default:
+                $o_column="id";
+                $o_order="DESC";
+                break;
+        }
+        $products=Product::orderBy('created_at','DESC')->orderBy($o_column,$o_order)->paginate(12);
+        return view('guest.shop',[
+            'product'=>$products,
+            'page'=>$page,
+            'size'=>$size,
+            'order'=>$order
+        ]);
     }
 
     public function detail($id){
-        return view('guest.productdetail');
+        $product=Product::where('id',$id)->first();
+        $rproducts =Product::where('id','!=',$id)->inRandomOrder('id')->get()->take(8);
+        return view('guest.productdetail',[
+            'product'=>$product,
+            'rproducts'=>$rproducts
+        ]);
     }
 
     public function showCompare(){
